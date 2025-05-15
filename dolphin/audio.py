6c90a6bf4a104777ad631ffcf2350b643d25a48a
yangyang <mengyangyang@dataoceanai.com>
2025-05-15 09:50:21 +0800

    convert audio to 16k

--- dolphin/audio.py
+++ dolphin/audio.py
@@ -35,3 +35,22 @@ def load_audio(file: str, sr: int = SAMPLE_RATE):
         raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
 
     return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
+
+
+def convert_audio(inp: str, outp: str):
+    try:
+        cmd = ['ffmpeg',
+            '-loglevel', '0',
+            '-nostdin',
+            '-threads', '0',
+            '-i', inp,
+            '-acodec', 'pcm_s16le',
+            '-ac', '1',
+            '-ar', '16000',
+            outp,
+            '-y'
+        ]
+        subprocess.run(cmd, capture_output=False, check=True)
+    except Exception as e:
+        logger.error(f"convert audio error, {e}")
+        raise RuntimeError(f"Failed to convert audio: {e.stderr.decode()}") from e
